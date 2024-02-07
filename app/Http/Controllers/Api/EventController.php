f6d3ffa7b6ea02853fb7a47aac38dff8cd0c9ed2
Giulia Ruggeri <giuliaruggeri94@hotmail.com>
2024-02-07 19:59:59 +0100

    Aggiunge tags

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -10,7 +10,7 @@ class EventController extends Controller
 {
     public function index()
     {
-        $results = Event::with("user")->get();
+        $results = Event::with(["user", "tags"])->get();
         $data = [
             "success" => true,
             "payload" => $results
